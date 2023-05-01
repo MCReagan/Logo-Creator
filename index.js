2c416e8dacb7e11a480e1ad713ee85277416f4c9
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-05-01 17:44:44 -0500

    not a working stage but have added a lot of js code

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'Enter one to three characters for your Logo.',
-            name: 'name',
-        },
-        {
-            type: 'input',
-            message: 'What color would you like your Logo to be?',
-            name: 'color',
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Choose a shape for your Logo.',
-            choices: ['Circle', 'Triangle', 'Square']
-        },
-    ])
-    .then(answers => {
-        console.log(answers.name, answers.color, answers.shape)
-    })
+function promptUser() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Enter one to three characters for your Logo.',
+                name: 'name',
+            },
+            {
+                type: 'input',
+                message: 'Enter color keyword or a hexadecimal number for your Logo TEXT.',
+                name: 'textColor',
+            },
+            {
+                type: 'list',
+                name: 'shape',
+                message: 'Choose a shape for your Logo.',
+                choices: ['Circle', 'Triangle', 'Square']
+            },
+            {
+                type: 'input',
+                message: 'Enter color keyword or a hexadecimal number for your Logo BACKGROUND.',
+                name: 'logoColor',
+            },
+        ])
+        .then(answers => {
+            if (answers.name.length > 3 || answers.name.length === 0) {
+                console.log("You must enter between 1 and 3 letters.")
+                promptUser();
+            }
+        })
+}
+
+promptUser();
+
+
+    // need to export answers to shape.js and finally to logo.js
