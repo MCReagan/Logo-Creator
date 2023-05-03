d172818f03cec49e03ca184bab6bb378ba0c9a0f
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-05-02 19:51:42 -0500

    added comments regarding functionality of code

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const promptUser = require('./lib/logo.js')
+// this imports the required js file to run promptUser()
+const { promptUser, setShape, makeLogo } = require('./lib/logo.js')
 
+// this starts promptUser on logo.js when node runs index.js
 promptUser();
 
